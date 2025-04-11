6d8660d7af3b8f62ba7e51c15426469887a67176
Hey wizard <95050077+cxinmayy@users.noreply.github.com>
2025-04-11 10:54:25 +0530

    Delete Seller/dbconnect.php

--- Seller/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server_name = 'localhost';
-$user_name = 'root';
-$user_password = '5555';
-$database_name = 'bcar1';
-
-$con = mysqli_connect($server_name, $user_name, $user_password, $database_name);
-?>
