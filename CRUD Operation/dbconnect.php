0c3440698ec7c36e93007fa2f47667a35c87ef5e
Hey wizard <95050077+cxinmayy@users.noreply.github.com>
2025-05-16 09:56:10 +0530

    Update and rename db.php to dbconnect.php

---
+++ CRUD Operation/dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+$server_name = 'localhost';
+$user_name = 'root';
+$user_password = '5555';
+$database_name = 'bcar1';
+
+$con = mysqli_connect($server_name, $user_name, $user_password, $database_name);
+?>
